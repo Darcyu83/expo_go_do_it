cabfad9db39b0abee12aabb22873518a6fb66d78
Darcyu83 <darcy.u83@gmail.com>
2022-11-04 15:53:08 +0900

    엑스포 앱 초기셋팅 및 로그인 화면

--- App.tsx
+++ App.tsx
@@ -1,20 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+  Button,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import InlineTextButton from './src/components/InlineTextButton';
+import { AppStyles } from './src/styles/appStyles';
+
+const localImage = require('./assets/bg_coffee.jpg');
 
 export default function App() {
+  const [userNm, setUserNm] = useState('');
+  const [userPw, setUserPw] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <ImageBackground source={localImage} style={AppStyles.container}>
+      <View style={AppStyles.backgroundCover}>
+        <Text style={[AppStyles.lightText, AppStyles.header]}>Login</Text>
+        {/* 아이디 */}
+        <TextInput
+          style={[
+            AppStyles.textInput,
+            AppStyles.lightText,
+            AppStyles.lightTextInput,
+          ]}
+          value={userNm}
+          onChangeText={setUserNm}
+          placeholder="Username"
+          placeholderTextColor={'#bebebe'}
+        />
+
+        {/* 비밀번호 */}
+        <TextInput
+          style={[
+            AppStyles.textInput,
+            AppStyles.lightText,
+            AppStyles.lightTextInput,
+          ]}
+          value={userPw}
+          onChangeText={setUserPw}
+          placeholder="Password"
+          placeholderTextColor={'#bebebe'}
+          secureTextEntry
+        />
+
+        {/* Sign Up 버튼 */}
+        <View style={[AppStyles.rowContainer]}>
+          <Text style={[AppStyles.lightText]}>Don't have an account? </Text>
+          <InlineTextButton title="Sign Up" onPress={() => {}} />
+        </View>
+
+        {/* Login 버튼 */}
+        <Button title="Login" color={'#ffd966'} />
+      </View>
+
       <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
